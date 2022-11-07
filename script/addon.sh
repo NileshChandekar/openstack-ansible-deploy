735b841c5604ad04e3b8e3e58157a99c5fb2163b
NileshChandekar <35838968+NileshChandekar@users.noreply.github.com>
2022-11-07 18:48:09 +0530

    Update addon.sh

--- script/addon.sh
+++ script/addon.sh
@@ -1,3 +1,7 @@
+adduser firoz
+echo "firoz ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/firoz
+
+
 ansible -i /ip.txt -m shell -a "ifdown enp2s0" all 
 ansible -i /ip.txt -m shell -a "route add default gw 192.168.122.1 enp1s0"  all 
 ansible -i /ip.txt -m shell -a "route -n"  all 
